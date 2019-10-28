bebbe7dd6a6eb2b6031c8b61194a9ac0c543bd4c
Leonid Petrov1 <leonid_petrov1@epam.com>
2019-10-28 20:28:33 +0400

    Add ascii charachers custom validation

---
+++ src/NorthwindStore.Data/Models/Validation/AsciiCharacters.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace NorthwindStore.Data.Models.Validation
+{
+    public class AsciiCharacters : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (!(value is string text))
+                return base.IsValid(value, validationContext);
+
+            return text.Any(x => x < 0 && x >= 128)
+                ? new ValidationResult(ErrorMessage)
+                : ValidationResult.Success;
+        }
+
+        public new string ErrorMessage => "Name should contain only ASCII characters";
+    }
+}
