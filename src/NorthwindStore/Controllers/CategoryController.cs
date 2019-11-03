3b3dbd0005efe624d07fc9af3af39450adc9623e
Leonid Petrov1 <leonid_petrov1@epam.com>
2019-11-03 22:57:04 +0400

    Add Edit page for Category with picture uploading feature

--- src/NorthwindStore/Controllers/CategoryController.cs
+++ src/NorthwindStore/Controllers/CategoryController.cs
@@ -1,6 +1,9 @@
-﻿using System;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using NorthwindStore.Data;
+using NorthwindStore.ViewModels;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace NorthwindStore.Controllers
 {
@@ -29,6 +32,46 @@ namespace NorthwindStore.Controllers
 
             byte[] image = categoryRepository.GetCategoryById(id).AlignedPicture;
             return File(image, "image/bmp");
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = categoryRepository.GetCategoryById(id);
+            var viewModel = new CategoryViewModel
+            {
+                CategoryModel = category,
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(CategoryViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                await using var ms = new MemoryStream();
+                await viewModel.CategoryImageFile.CopyToAsync(ms);
+                if (ms.Length >= Conventions.FileUploading.BufferedMaxSize)
+                {
+                    ModelState.AddModelError("CategoryModel.Picture", "The file is too large");
+                    return View(viewModel);
+                }
+
+                viewModel.CategoryModel.UploadPicture(ms.ToArray());
+                categoryRepository.UpdateCategory(viewModel.CategoryModel);
+                categoryRepository.Save();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(viewModel);
         }
     }
 }
---
+++ src/NorthwindStore/Conventions/FileUploading.cs
@@ -0,0 +1,7 @@
+﻿namespace NorthwindStore.Conventions
+{
+    public static class FileUploading
+    {
+        public const int BufferedMaxSize = 2 * 1024 * 1024;
+    }
+}
