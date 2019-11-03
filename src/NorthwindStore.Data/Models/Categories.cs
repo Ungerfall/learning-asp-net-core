818ea8d46bb2a408abd4b57555b177a94be599aa
Leonid Petrov1 <leonid_petrov1@epam.com>
2019-11-03 19:45:27 +0400

    Align Picture to skip first 78 bytes
    Northwind images were created with Microsoft Access so they have a 78 byte OLE header

--- src/NorthwindStore.Data/Models/Categories.cs
+++ src/NorthwindStore.Data/Models/Categories.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
 
 namespace NorthwindStore.Data.Models
 {
@@ -14,6 +16,23 @@ namespace NorthwindStore.Data.Models
         public string CategoryName { get; set; }
         public string Description { get; set; }
         public byte[] Picture { get; set; }
+
+        /// <summary>
+        /// Gets aligned <see cref="Picture"/>. Northwind images were created
+        /// with Microsoft Access so they have a 78 byte OLE header.
+        /// </summary>
+        [NotMapped]
+        public byte[] AlignedPicture
+        {
+            get
+            {
+                using var ms = new MemoryStream();
+                const int offset = 78;
+                ms.Write(Picture, offset, Picture.Length - offset);
+
+                return ms.ToArray();
+            }
+        }
 
         public virtual ICollection<Products> Products { get; set; }
     }
