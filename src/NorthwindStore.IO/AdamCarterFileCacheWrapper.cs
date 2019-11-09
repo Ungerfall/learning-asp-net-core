0230de79e3420b5a52b9ff55ccd32adef33037fb
Leonid Petrov1 <leonid_petrov1@epam.com>
2019-11-09 21:50:09 +0400

    Add 3rd party file cache

---
+++ src/NorthwindStore.IO/AdamCarterFileCacheWrapper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Caching;
+using System.Threading;
+
+namespace NorthwindStore.IO
+{
+    public sealed class AdamCarterFileCacheWrapper : IFileCache
+    {
+        private static object @lock = new object();
+
+        private readonly FileCache cache;
+
+        public AdamCarterFileCacheWrapper()
+        {
+            cache = new FileCache(
+                cacheRoot: Configuration.Dir,
+                calculateCacheSize: false,
+                cleanInterval: Configuration.ExpirationTime);
+        }
+
+        public MemoryStream GetOrCreate(string key, Func<MemoryStream> factory)
+        {
+            lock (@lock)
+            {
+                if (cache.Contains(key))
+                {
+                    var cachedBytes = cache[key] as byte[];
+                    if (cachedBytes == null)
+                        return null;
+
+                    return new MemoryStream(cachedBytes);
+                }
+            }
+
+
+            lock (@lock)
+            {
+                var cachedItems = cache.GetCount();
+                if (cachedItems >= Configuration.MaxCachedCount)
+                    return null;
+
+                var memoryStream = factory.Invoke();
+                cache.Add(
+                    new CacheItem(key, memoryStream.ToArray()),
+                    new CacheItemPolicy
+                    {
+                        AbsoluteExpiration = DateTimeOffset.Now.Add(Configuration.ExpirationTime)
+                    });
+
+                return memoryStream;
+            }
+        }
+
+        public FileCacheConfiguration Configuration { get; set; } = new FileCacheConfiguration
+        {
+            Dir = "/image-cache",
+            ExpirationTime = Timeout.InfiniteTimeSpan,
+            MaxCachedCount = FileCacheConfiguration.INFINITY_CACHE_ENTRIES
+        };
+    }
+}
---
+++ src/NorthwindStore.IO/IFileCache.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.IO;
+
+namespace NorthwindStore.IO
+{
+    public interface IFileCache
+    {
+        MemoryStream GetOrCreate(string key, Func<MemoryStream> factory);
+    }
+}
