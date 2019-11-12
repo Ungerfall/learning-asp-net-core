df9e865f4cff40f7c71f3355818b61270fb4a6e2
Leonid Petrov1 <leonid_petrov1@epam.com>
2019-11-13 02:10:50 +0400

    Add image regex patterns being used in the app

---
+++ src/NorthwindStore/Conventions/ImageRequestPatterns.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace NorthwindStore.Conventions
+{
+    public class ImageRequestPatterns : IEnumerable<Regex>
+    {
+        public static IReadOnlyCollection<Regex> Collection = new List<Regex>
+        {
+            new Regex(@"^.+/images/\d+", RegexOptions.Compiled),
+            new Regex($@"^.+\.({ImageFormats.AsRegexpOrSubexpression})", RegexOptions.Compiled)
+        };
+
+        public IEnumerator<Regex> GetEnumerator()
+        {
+            return Collection.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
