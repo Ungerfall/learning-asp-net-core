cd16d5f653bebb3f7fb363c747d58212134de76b
Leonid Petrov1 <leonid_petrov1@epam.com>
2019-11-17 02:17:09 +0400

    Add extension method to select key-value parameters from FilterContext

---
+++ src/NorthwindStore/Extensions/FilterContextExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NorthwindStore.Extensions
+{
+    public static class FilterContextExtensions
+    {
+        public static IReadOnlyDictionary<string, string> GetActionParameters(this ResultExecutedContext context)
+        {
+            var ctrl = context.Controller as Controller;
+            return ctrl == null
+                ? new Dictionary<string, string>()
+                : GetActionParameters(ctrl);
+        }
+
+        public static IReadOnlyDictionary<string, string> GetActionParameters(this ResultExecutingContext context)
+        {
+            var ctrl = context.Controller as Controller;
+            return ctrl == null
+                ? new Dictionary<string, string>()
+                : GetActionParameters(ctrl);
+        }
+
+        private static IReadOnlyDictionary<string, string> GetActionParameters(Controller ctrl)
+        {
+            return ctrl
+                .ControllerContext
+                .ActionDescriptor
+                .Parameters
+                .ToDictionary(x => x.Name, x => ctrl.RouteData.Values[x.Name].ToString());
+        }
+    }
+}
