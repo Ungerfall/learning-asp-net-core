5e39f6eb12ee83c613c4dacfda083a0b869b7466
Leonid Petrov1 <leonid_petrov1@epam.com>
2019-12-15 21:10:36 +0400

    Add northing image tag helper

---
+++ src/NorthwindStore/TagHelpers/NorthwindImageTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace NorthwindStore.TagHelpers
+{
+    [HtmlTargetElement("a", Attributes = "northwind-id")]
+    public class NorthwindImageTagHelper : TagHelper
+    {
+        [HtmlAttributeName("northwind-id")]
+        public int NorthwindId { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.RemoveAll("href");
+            output.Attributes.SetAttribute("href", "images/" + NorthwindId);
+        }
+    }
+}
