bcf38ff7bd6b58f616e023dcf9a15cc208c61213
Leonid Petrov1 <leonid_petrov1@epam.com>
2019-12-29 14:59:39 +0400

    Register northwind context

--- src/NorthwindStore.API/Startup.cs
+++ src/NorthwindStore.API/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NorthwindStore.Data.Models;
 
 namespace NorthwindStore.API
 {
@@ -26,6 +28,8 @@ namespace NorthwindStore.API
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllers();
+            services.AddDbContext<NorthwindContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("NorthwindContext")));
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
