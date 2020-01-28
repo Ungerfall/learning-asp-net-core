f05e8a709f99389a5a70a1eaa6db20e685f9528a
Leonid Petrov1 <leonid_petrov1@epam.com>
2020-01-28 10:28:20 +0400

    Add adminstator contoller

---
+++ src/NorthwindStore/Controllers/AdministratorController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NorthwindStore.Data.Models;
+using NorthwindStore.ViewModels;
+using System.Linq;
+
+namespace NorthwindStore.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class AdministratorController : Controller
+    {
+        private readonly NorthwindContext dboContext;
+
+        public AdministratorController(NorthwindContext dboContext)
+        {
+            this.dboContext = dboContext;
+        }
+
+        // GET
+        public IActionResult Index()
+        {
+            return View(dboContext.Users
+                .Select(x => new AdministratorViewModel
+                {
+                    UserId = x.Id,
+                    UserName = x.UserName,
+                    UserEmail = x.Email
+                }));
+        }
+    }
+}
